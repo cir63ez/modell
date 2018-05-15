9ae4bee0aba11599d22990073205c6d8b0e0d56c
Gautier Miquet <gautier.miquet@isen.yncrea.fr>
2018-05-15 10:54:15 +0200

    Updated libs

--- renderer/ellipse.h
+++ renderer/ellipse.h
@@ -1,15 +1,10 @@
 #include "lib.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-#define NaN 0./0.
 
 typedef struct Ellipse_ {
     double a;
     double b;
     double c;
-}Ellipse;
+} Ellipse;
 
 Point contactEllipseWithLine(Ellipse E, Line L);
 
