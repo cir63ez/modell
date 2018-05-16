f24fdf72b8a04732864b60a6d9592c2db0fcc209
Gautier Miquet <gautier.miquet@isen.yncrea.fr>
2018-05-16 14:11:49 +0200

    Fixed list

--- renderer/list.h
+++ renderer/list.h
@@ -1,19 +1,17 @@
 #include "lib.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
 
 #define ELLIPSE_TYPE 0
 #define BRICK_TYPE 1
 
-typedef struct Element_ {
+typedef struct Element_
+{
     char type;
-    double * object;
-    struct Element_ * next;
+    double *object;
+    struct Element_ *next;
 } Element;
 
-typedef struct List_ {
-    Element * tete;
+typedef struct List_
+{
+    Element *tete;
     double nbElement;
 } List;
-
