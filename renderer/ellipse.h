dae15b8bb16fee83801313f2b3507b65740ba8ad
Remi_A <remi.arbache@isen.yncrea.fr>
2018-05-16 11:11:24 +0200

    Commented

--- renderer/ellipse.h
+++ renderer/ellipse.h
@@ -1,9 +1,11 @@
 #include "lib.h"
 
 typedef struct Ellipse_ {
+    //radii of the ellipsoid
     double a;
     double b;
     double c;
+    // x y z coordinates of the center point
     double x;
     double y;
     double z;
