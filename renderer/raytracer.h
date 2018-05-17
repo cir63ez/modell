ebb3bf24effee5bc7fc43fc83abe72201fb3b024
Marc Caboche <ychixm@Marc.C>
2018-05-17 11:10:30 +0200

    add some description

--- renderer/raytracer.h
+++ renderer/raytracer.h
@@ -2,5 +2,8 @@
 #include "ellipse.h"
 #include "bmp.h"
 
+//Calculates the ray corresponding to the (0,0) pixel
 Line calculateFirstRay (Plane image, Point origin);
+
+//(test version) calculate pixel's color
 void rayTracer(Ellipse E, /*Light *list,*/ Plane observer, Point imageOrigin, int resolution);
