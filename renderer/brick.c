19669d3cc7837fe3f34a82884e9dd7420d082997
Julia Mourier <julia.mourier@isen.yncrea.fr>
2018-05-18 14:55:28 +0200

    Update brick function list

--- renderer/brick.c
+++ renderer/brick.c
@@ -321,7 +321,6 @@ int testIfLightCutsBrick(double *object,Light Li,Point C) {
     else {
         return FALSE;
     }
-<<<<<<< HEAD
 }
 
 /**
@@ -341,6 +340,3 @@ Element * createElementBrick(Brick B) {
         element->next = NULL;
     }
 }
-=======
-}
->>>>>>> fa9b9e4e1b652d62466bf748e5c7434e46d38b8c
