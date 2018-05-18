805c1924da02737b155f846cbb1a663db549712b
Remi_A <remi.arbache@isen.yncrea.fr>
2018-05-18 10:43:13 +0200

    Minor fixes

--- renderer/tetrahedron.c
+++ renderer/tetrahedron.c
@@ -1,12 +1,9 @@
 #pragma once
-<<<<<<< HEAD
-=======
 #include "lib.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 #include "list.h"
->>>>>>> 5b502f1d0048de04ebe3b79719e49277e0562c23
 #include "tetrahedron.h"
 
 /**
