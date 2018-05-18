6516aace8acd4bcf9727577c4f70858ebd9ebf0b
Julia Mourier <julia.mourier@isen.yncrea.fr>
2018-05-18 14:42:20 +0200

    Update Slogan

--- server/views/home.php
+++ server/views/home.php
@@ -2,9 +2,9 @@
 
 <div class="jumbotron">
     <h1 class="display-3"><?= APP_NAME ?></h1>
-    <p class="lead">TODO: Descriptive text of the project</p>
+    <p class="lead">CIR1 Image Sythesis Project</p>
     <hr class="my-4">
-    <p>May the 4th be with you - TODO: Maybe another slogan ?</p>
+    <p>May the 4th be with you - EZ Tracer</p>
     <p class="lead">
         <a class="btn btn-primary btn-lg" href="./?app" role="button">Use Modell</a>
     </p>
