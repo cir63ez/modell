e2ffe41f5768050db298d04d659e9bf9f346b8cb
Gautier Miquet <gautier.miquet@isen.yncrea.fr>
2018-05-22 15:16:56 +0200

    PHP Minor fixs & generilized cube to brick

--- server/controllers/process.php
+++ server/controllers/process.php
@@ -11,14 +11,21 @@ if(!isset($_POST) || (sizeof($_POST) == 0)) {
     die('No form submitted.<br> <a href="?app">Back to Modell</a>');
 }
 
+// Remove previous files
 if(file_exists($dataFile)) { unlink($dataFile); }
 if(file_exists($imageFile)) { unlink($imageFile); }
 
+// Seralize data & prepare input file
 $data = serializeForm($_POST);
 file_put_contents($dataFile, $data);
 
+var_dump($data);
+
+// Execute the renderer
 $output = exec($rendererExecutable);
 
+// Dump output
 var_dump($output);
 
+// Show image
 echo '<img src="' . $imageFile . '" alt="">';
