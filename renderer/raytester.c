90a35e69a7ce068c973f19ffad7a8d4253357a2d
Remi_A <remi.arbache@isen.yncrea.fr>
2018-05-24 23:20:05 +0200

    Testing and tweaking

--- renderer/raytester.c
+++ renderer/raytester.c
@@ -28,26 +28,26 @@ int main(){
     */
 
     Ellipse eA;
-    eA.a =  eA.b = eA.c = 500;
+    eA.a =  eA.b = eA.c = 100;
 
-    eA.x = eA.y = 0;
-    eA.z = 0;
+    eA.x = eA.y = 120;
+    eA.z = 100000000;
 
-    // Ellipse eB;
-    // eB.a =  eB.b = eB.c = 100;
-    //
-    // eB.x = eB.y = 0;
-    // eB.z = 0;
+    Ellipse eB;
+    eB.a =  eB.b = eB.c = 500;
+
+    eB.x = eB.y = 0;
+    eB.z = 100000000;
 
 
     List *list;
     list = initList();
-    addElementList(createElementEllipse(encodeEllipse(eA)), list);
-    // addElementList(createElementEllipse(encodeEllipse(eB)), list);
+    addElementList(createElementEllipse(encodeEllipse(eB)), list);
+    // addElementList(createElementEllipse(encodeEllipse(eA)), list);
     // addElementList(createElementBrick(encodeBrick(object)), list);
 
     Light hope;
-    hope.lightSource = setPoint(50000, 0, 0);
+    hope.lightSource = setPoint(1000000000, 0, 100000000);
 
     Light *lightList;
     lightList = (Light*)malloc(sizeof(Light));
@@ -56,17 +56,22 @@ int main(){
     Plane observer;
     observer.a = observer.b = 0;
     observer.c = 1;
-    observer.x = observer.y = 0;
-    observer.z = -10000;
+    observer.x = 10000000000;
+    observer.y = 0;
+    observer.z = 100000000;
 
     Point imageOrigin;
     imageOrigin.x = -500;
     imageOrigin.y =  500;
-    imageOrigin.z =-1000000;
+    imageOrigin.z =   0;
 
     int resolution = 1000;
 
-    rayTracer(list, lightList, observer, imageOrigin, resolution);
+    double tetaX =  0;
+    double tetaY = 0;
+    double tetaZ =  0;
+
+    rayTracer(list, lightList, observer, imageOrigin, resolution, tetaX, tetaY, tetaZ);
 
     return 0;
 }
